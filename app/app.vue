6990100557e2098ddc3f6edcbad4af4fcebbe1fe
mulumpyo <1119_dh@naver.com>
2025-10-14 19:59:57 +0900

    init

---
+++ app/app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import axios from 'axios';
+
+onMounted(async () => {
+  const res = await axios.get('/api/helloWorld');
+  console.log(res);
+});
+
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
